d5ad8a07c3a41cd32d1f385d290b241848255c67
kent <s9n9201@yahoo.com.tw>
2022-04-19 20:01:34 +0800

    Add @ControllerAdvice

--- src/main/java/web/fileupload/exception/FileUploadExceptionAdvice.java
+++ src/main/java/web/fileupload/exception/FileUploadExceptionAdvice.java
@@ -2,11 +2,13 @@ package web.fileupload.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import web.fileupload.message.ResponseMessage;
 
+@ControllerAdvice
 public class FileUploadExceptionAdvice extends ResponseEntityExceptionHandler {
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     public ResponseEntity<ResponseMessage> handleMaxSizeException(MaxUploadSizeExceededException e) {

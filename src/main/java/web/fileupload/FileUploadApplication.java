edcd07137010f74e986f7ff1b1d1854158d2295f
kent <s9n9201@yahoo.com.tw>
2022-04-19 16:33:34 +0800

    implements CommandLineRunner override run method

--- src/main/java/web/fileupload/FileUploadApplication.java
+++ src/main/java/web/fileupload/FileUploadApplication.java
@@ -1,13 +1,24 @@
 package web.fileupload;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import web.fileupload.service.FilesStorageService;
 
 @SpringBootApplication
-public class FileUploadApplication {
+public class FileUploadApplication implements CommandLineRunner {
+
+    @Autowired
+    FilesStorageService filesStorageService;
 
     public static void main(String[] args) {
         SpringApplication.run(FileUploadApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        filesStorageService.deleteAll();
+        filesStorageService.init();
+    }
 }

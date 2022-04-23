5122fe3dba38234cae6c796534b4427f07ea8815
kent <s9n9201@yahoo.com.tw>
2022-04-23 21:18:41 +0800

    remove out.print

--- src/main/java/web/fileupload/exception/FileUploadExceptionAdvice.java
+++ src/main/java/web/fileupload/exception/FileUploadExceptionAdvice.java
@@ -15,7 +15,6 @@ public class FileUploadExceptionAdvice extends ResponseEntityExceptionHandler {
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     public ResponseEntity<ResponseMessage> handleMaxSizeException(MaxUploadSizeExceededException e, HttpServletResponse response) {
         response.setHeader("Access-Control-Allow-Origin", "*"); //需要加這個，才不會因為檔案超過大小後，前端出現CORS的異常，才能正常噴出Response內容
-        System.out.println(e.getMessage());
         return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage("檔案太大了！"));
     }
 }

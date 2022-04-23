c5e6e9a6925da34537e19a5f7fda740ad0e53124
kent <s9n9201@yahoo.com.tw>
2022-04-23 21:18:23 +0800

    set responseHeader

--- src/main/java/web/fileupload/exception/FileUploadExceptionAdvice.java
+++ src/main/java/web/fileupload/exception/FileUploadExceptionAdvice.java
@@ -8,10 +8,14 @@ import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import web.fileupload.message.ResponseMessage;
 
+import javax.servlet.http.HttpServletResponse;
+
 @ControllerAdvice
 public class FileUploadExceptionAdvice extends ResponseEntityExceptionHandler {
     @ExceptionHandler(MaxUploadSizeExceededException.class)
-    public ResponseEntity<ResponseMessage> handleMaxSizeException(MaxUploadSizeExceededException e) {
+    public ResponseEntity<ResponseMessage> handleMaxSizeException(MaxUploadSizeExceededException e, HttpServletResponse response) {
+        response.setHeader("Access-Control-Allow-Origin", "*"); //需要加這個，才不會因為檔案超過大小後，前端出現CORS的異常，才能正常噴出Response內容
+        System.out.println(e.getMessage());
         return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage("檔案太大了！"));
     }
 }
